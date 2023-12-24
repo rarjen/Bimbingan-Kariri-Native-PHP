c3ad347b8cda3e0c54243449ddf9661d6e302434
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-12-24 22:38:20 +0700

    delete contentwrapper

--- components/contentwrapper.php
+++
@@ -1,7 +0,0 @@
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <?php include("pages/obat/obat.php") ?>
-    </div>
-    <!-- /.content -->
-</div>
