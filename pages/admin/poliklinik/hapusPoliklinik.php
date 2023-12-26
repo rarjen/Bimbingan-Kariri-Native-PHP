5c0d162c2b024563374b5122b89e8fba050b6139
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-12-26 19:56:08 +0700

    create hapusPoli

---
+++ pages/admin/poliklinik/hapusPoliklinik.php
@@ -0,0 +1,9 @@
+<?php
+include '../../../koneksi.php'; //menghubungkan ke file koneksi untuk ke database
+$id = $_GET['id']; //menampung id
+
+//query hapus
+$datas = mysqli_query($mysqli, "DELETE FROM poli WHERE id ='$id'") or die(mysqli_error($mysqli));
+
+//alert dan redirect ke obat.php
+echo "<script>alert('data berhasil dihapus.');window.location='./index.php';</script>";
