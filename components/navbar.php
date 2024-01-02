4e44ce0ce6942eb42634565c21b66594d9164494
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-01-02 13:02:27 +0700

    modify navbar

--- components/navbar.php
+++ components/navbar.php
@@ -12,7 +12,7 @@
             <a href="#" class="nav-link">Contact</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Logout</a>
+            <a href="../utils/logout.php" class="nav-link">Logout</a>
         </li>
     </ul>
 
