97030763d8840871d8fea6b86e02b43e752d9d6a
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-01-06 10:37:26 +0700

    add redirect

--- index.php
+++ index.php
@@ -7,6 +7,14 @@ $redirect = null;
 if (isset($_SESSION['login'])) {
     $show = true;
     $redirect = $_SESSION['akses'];
+
+    if ($redirect == "admin") {
+        echo "<meta http-equiv='refresh' content='0; url=pages/admin/index.php'>";
+    } else if ($redirect == "pasien") {
+        echo "<meta http-equiv='refresh' content='0; url=pages/pasien/index.php'>";
+    } else {
+        echo "<meta http-equiv='refresh' content='0; url=pages/dokter/index.php'>";
+    }
 }
 
 if ($show) {
