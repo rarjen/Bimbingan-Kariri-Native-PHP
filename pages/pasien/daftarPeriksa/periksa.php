6f6504f7cc9f33d8e85ea8f6dcdf6011c2acda6f
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-01-06 10:37:44 +0700

    add session

--- pages/pasien/daftarPeriksa/periksa.php
+++ pages/pasien/daftarPeriksa/periksa.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+$no_rm = $_SESSION['no_rm'];
+$id = $_SESSION['id'];
+$id_poli = 0;
+?>
 <div class="d-flex">
     <div class="col-4">
         <div class="card border rounded-lg" style="background-color: white; overflow: hidden;">
@@ -7,7 +13,7 @@
             <form action="daftarPeriksa/tambahPeriksa.php" method="POST" class="py-2 px-3">
                 <div class="form-group my-2">
                     <label for="no_rm">Nomor Rekam Medis</label>
-                    <input class="w-100 px-4 rounded-lg border page-link" type="text" name="no_rm" id="no_rm" placeholder="202312-001" disabled required>
+                    <input class="w-100 px-4 rounded-lg border page-link text-dark" type="text" name="no_rm" id="no_rm" placeholder="202312-001" value="<?= $no_rm; ?>" disabled required>
                 </div>
                 <div class="form-group my-2">
                     <label for="no_rm">Pilih Poliklinik</label>
@@ -17,6 +23,7 @@
                         $resultPoli = mysqli_query($mysqli, $queryPoli);
                         while ($rowPoli = mysqli_fetch_assoc($resultPoli)) {
                             echo "<option value='{$rowPoli['id']}'>{$rowPoli['nama_poli']}</option>";
+                            $id_poli = $rowPoli['id'];
                         }
                         ?>
                     </select>
@@ -28,6 +35,7 @@
                         $queryJadwal = "SELECT jadwal_periksa.id, dokter.nama AS nama_dokter, jadwal_periksa.hari, jadwal_periksa.jam_mulai, jadwal_periksa.jam_selesai
                         FROM jadwal_periksa
                         JOIN dokter ON jadwal_periksa.id_dokter = dokter.id";
+
                         $resultJadwal = mysqli_query($mysqli, $queryJadwal);
 
                         while ($rowJadwal = mysqli_fetch_assoc($resultJadwal)) {
@@ -72,6 +80,37 @@
                         </tr>
                     </thead>
                     <tbody>
+                        <?php
+                        $query = "SELECT dp.id_pasien, pl.nama_poli, d.nama, jp.hari, jp.jam_mulai, jp.jam_selesai, dp.no_antrian
+                                  FROM daftar_poli AS dp
+                                  JOIN pasien AS p ON dp.id_pasien = p.id
+                                  JOIN jadwal_periksa AS jp ON dp.id_jadwal = jp.id
+                                  JOIN dokter AS d ON jp.id_dokter = d.id
+                                  JOIN poli AS pl ON d.id_poli = pl.id
+                                  WHERE dp.id_pasien = $id;";
+                        $result = mysqli_query($mysqli, $query);
+                        $no = 1;
+
+                        while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
+
+                            <tr>
+                                <td><?= $no++; ?></td>
+                                <td><?= $row['nama_poli']; ?></td>
+                                <td><?= $row['nama']; ?></td>
+                                <td><?= $row['hari']; ?></td>
+                                <td><?= $row['jam_mulai']; ?></td>
+                                <td><?= $row['jam_selesai']; ?></td>
+                                <td><?= $row['no_antrian']; ?></td>
+                                <td>
+                                    <button type='button' class='btn btn-sm btn-info edit-btn' data-obatid='<?php echo $row['id']; ?>'>Detail</button>
+                                </td>
+                            </tr>
+
+                        <?php }
+                        mysqli_close($mysqli)
+                        ?>
+
                     </tbody>
                     <script>
                     </script>
