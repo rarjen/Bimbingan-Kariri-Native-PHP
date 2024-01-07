6879dc71782e6f52f568ac94a22cb19f33520188
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-01-07 21:14:23 +0700

    modify update jadwal

---
+++ pages/dokter/jadwal/updateJadwal.php
@@ -0,0 +1,49 @@
+<?php
+include("../../../koneksi.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $id = $_POST["id"];
+    $hari = $_POST["hari"];
+    $jam_mulai = $_POST["jam_mulai"];
+    $jam_selesai = $_POST["jam_selesai"];
+
+    $queryCheckExist = "SELECT isUsed FROM jadwal_periksa WHERE id = '$id'";
+    $resultCheckExist = mysqli_query($mysqli, $queryCheckExist);
+
+    if ($resultCheckExist) {
+        $rowCheckExist = mysqli_fetch_assoc($resultCheckExist);
+
+        if ($rowCheckExist['isUsed'] == 1) {
+            echo '<script>alert("Jadwal tidak dapat diubah karena sudah digunakan.");</script>';
+            echo "<meta http-equiv='refresh' content='0; url=../../../index.php'>";
+        } else {
+            $query = "UPDATE jadwal_periksa SET
+                        hari = ?,
+                        jam_mulai = ?,
+                        jam_selesai = ?
+                        WHERE id = ?";
+
+            $stmt = mysqli_prepare($mysqli, $query);
+
+            mysqli_stmt_bind_param($stmt, "sssi", $hari, $jam_mulai, $jam_selesai, $id);
+
+            // Eksekusi query
+            if (mysqli_stmt_execute($stmt)) {
+                // Jika berhasil, redirect kembali ke halaman index atau sesuaikan dengan kebutuhan Anda
+                echo '<script>';
+                echo 'alert("Data jadwal berhasil diubah!");';
+                echo 'window.location.href = "../../../index.php";';
+                echo '</script>';
+                exit();
+            } else {
+                // Jika terjadi kesalahan, tampilkan pesan error
+                echo "Error: " . $query . "<br>" . mysqli_error($mysqli);
+            }
+
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    mysqli_close($mysqli);
+}
