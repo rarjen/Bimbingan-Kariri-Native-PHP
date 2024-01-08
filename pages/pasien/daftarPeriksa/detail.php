c55f66101223522446cb1c1595fa155b2abbd5b5
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-01-08 08:28:19 +0700

    modify detail

--- pages/pasien/daftarPeriksa/detail.php
+++ pages/pasien/daftarPeriksa/detail.php
@@ -1,93 +1,57 @@
 <?php
+include("../../../koneksi.php");
 
-include_once("../../../koneksi.php");
+$id = $_GET['id'];
 
-$urlParts = explode('/', $_SERVER['REQUEST_URI']);
-$id = end($urlParts);
+$query = "SELECT dp.id, dp.id_pasien, pl.nama_poli, d.nama, jp.hari, jp.jam_mulai, jp.jam_selesai, dp.no_antrian
+FROM daftar_poli AS dp
+JOIN pasien AS p ON dp.id_pasien = p.id
+JOIN jadwal_periksa AS jp ON dp.id_jadwal = jp.id
+JOIN dokter AS d ON jp.id_dokter = d.id
+JOIN poli AS pl ON d.id_poli = pl.id
+WHERE dp.id = $id;";
+$result = mysqli_query($mysqli, $query);
 
+$row = mysqli_fetch_assoc($result);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detail Pendaftaran</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-    <style>
-        p {
-            font-style: bold;
-            color: grey;
-            font-size: 18px;
-        }
-    </style>
-</head>
-
-<body style="background-color: #F0F8FF;">
-    <div class="container">
-        <div>
-            <div class="col-8 mt-5 mx-auto">
-                <div class="card shadow bg-body-tertiary rounded">
-                    <div class="card-header mb-3" style="background-color: #48829E;">
-                        <h5 class="card-title text-white my-auto">Detail Jadwal Periksa</h5>
+<!-- Modal -->
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editModalLabel">Detail Periksa</h5>
+            </div>
+            <div class="modal-body">
+                <form id="editForm" method="POST" action="obat/updateObat.php">
+                    <input type="hidden" name="id" value="<?= $row['id']; ?>">
+                    <div class="form-group">
+                        <label for="nama_poliklinik">Nama Poliklinik</label>
+                        <input type="text" class="form-control" id="nama_poliklinik" name="nama_poliklinik" value="<?= $row['nama_poli']; ?>" disabled>
                     </div>
-                    <!-- /.card-header -->
-                    <div class="card-body table-responsive p-0">
-                        <table class="table table-hover text-nowrap">
-                            <tbody>
-                                <?php
-                                $query = "SELECT dp.id, dp.id_pasien, pl.nama_poli, d.nama, jp.hari, jp.jam_mulai, jp.jam_selesai, dp.no_antrian
-                                    FROM daftar_poli AS dp
-                                    JOIN pasien AS p ON dp.id_pasien = p.id
-                                    JOIN jadwal_periksa AS jp ON dp.id_jadwal = jp.id
-                                    JOIN dokter AS d ON jp.id_dokter = d.id
-                                    JOIN poli AS pl ON d.id_poli = pl.id
-                                    WHERE dp.id = $id;";
-                                $result = mysqli_query($mysqli, $query);
-
-                                $row = mysqli_fetch_assoc($result);
-
-                                if ($row) {
-
-                                ?>
-                                    <div class="col-12 text-center">
-                                        <h5>Nama Poliklinik</h5>
-                                        <p><?= $row['nama_poli']; ?></p>
-                                        <hr>
-                                        <h5>Nama Dokter</h5>
-                                        <p><?= $row['nama']; ?></p>
-                                        <hr>
-                                        <h5>Hari</h5>
-                                        <p><?= $row['hari']; ?></p>
-                                        <hr>
-                                        <h5>Jam Mulai</h5>
-                                        <p><?= $row['jam_mulai']; ?></p>
-                                        <hr>
-                                        <h5>Jam Selesai</h5>
-                                        <p><?= $row['jam_selesai']; ?></p>
-                                        <hr>
-                                        <h5>No Antrian</h5>
-                                        <p><?= $row['no_antrian']; ?></p>
-                                    </div>
-                                <?php
-                                } else {
-                                    // Data tidak ditemukan atau terjadi kesalahan
-                                    echo "Data tidak ditemukan atau terjadi kesalahan.";
-                                }
-                                mysqli_close($mysqli);
-                                ?>
-
-                            </tbody>
-                        </table>
+                    <div class="form-group">
+                        <label for="nama_dokter">Nama Dokter</label>
+                        <input type="text" class="form-control" id="nama_dokter" name="nama_dokter" value="<?= $row['nama']; ?>" disabled>
                     </div>
-                    <!-- /.card-body -->
-                </div>
+                    <div class="form-group">
+                        <label for="hari">Hari</label>
+                        <input type="text" class="form-control" id="hari" name="hari" value="<?= $row['hari']; ?>" disabled>
+                    </div>
+                    <div class="form-group">
+                        <label for="jam_mulai">Jam Mulai</label>
+                        <input type="text" class="form-control" id="jam_mulai" name="jam_mulai" value="<?= $row['jam_mulai']; ?>" disabled>
+                    </div>
+                    <div class="form-group">
+                        <label for="jam_selesai">Jam Selesai</label>
+                        <input type="text" class="form-control" id="jam_selesai" name="jam_selesai" value="<?= $row['jam_selesai']; ?>" disabled>
+                    </div>
+                    <div class="form-group">
+                        <label for="no_antrian">No Antrian</label>
+                        <input type="text" class="form-control" id="no_antrian" name="no_antrian" value="<?= $row['no_antrian']; ?>" disabled>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-
-</html>
+</div>
