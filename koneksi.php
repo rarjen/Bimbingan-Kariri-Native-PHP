f6d6e628e4b08fe631131293cfb32b189fbf2e1d
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-01-08 18:43:40 +0700

    create function query

--- koneksi.php
+++ koneksi.php
@@ -15,3 +15,15 @@ $mysqli = mysqli_connect(
 if (!$mysqli) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
+function query($query)
+{
+    global $mysqli;
+    $result = mysqli_query($mysqli, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
