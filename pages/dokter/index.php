6304fc2ce06b5eea84317a32e3412c4dcf66c82d
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-01-08 18:43:55 +0700

    modify index dokter

--- pages/dokter/index.php
+++ pages/dokter/index.php
@@ -40,6 +40,7 @@
     <link rel="stylesheet" href="../../assets/admin/dist/css/adminlte.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
 </head>
 
 <body class="hold-transition sidebar-mini">
